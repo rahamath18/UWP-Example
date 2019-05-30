1a96cfca824d16c14e9e64798d6406bb3a943af0
Rahamathullah S <rahamath18@yahoo.com>
2019-05-30 16:57:04 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+Download Visual Studio 2019 for Xamarin: 
+	https://visualstudio.microsoft.com/xamarin/
+	Install: Universal Windows Platform development
+
+Install Guide for 
+	https://docs.microsoft.com/en-us/xamarin/get-started/installation/index?tabs=windows
+
+Universal Windows Platform (UWP) Tutorial
+	https://www.tutorialspoint.com/windows10_development/windows10_development_uwp.htm
---
+++ UWP-Example/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_Example
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                try
+                {
+                    Int32 result = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
+                    txt3.Text = "Result: " + result;
+                }
+                catch (Exception ex)
+                {
+                    txt3.Text = "Only enter number";
+                }
+            }
+            else
+                txt3.Text = "You have not entered 1st or 2nd number";
+        }
+    }
+}
